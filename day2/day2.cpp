254d56ab6734a27077f4a5fcfdb60adc1f22ce8f
Andrej Pištek <andrej.pistek@outlook.com>
2022-12-07 16:41:07 +0100

    day 2 part 2

--- day2/day2.cpp
+++ day2/day2.cpp
@@ -18,6 +18,36 @@ int calculate_strategy_score(const std::vector<std::pair<char,char>>& rounds) {
     });
 }
 
+int calculate_new_strategy_score(const std::vector<std::pair<char,char>>& rounds) {
+    return std::accumulate(rounds.begin(), rounds.end(), 0, [](int accumulator, auto& pair) -> int {
+        // 1 == rock, 2 == paper, 3 == scissors
+        int opponent = pair.first - 'A' + 1;
+        // 1 == X == lose, 2 == Y == draw, 3 == Z == win
+        int me = pair.second - 'X' + 1;
+
+        if (me == 1) { // lose
+            if (opponent == 1) {
+                return accumulator + 3;
+            }else if (opponent == 2) {
+                return accumulator + 1;
+            }else {
+                return accumulator + 2;
+            }
+            return accumulator + me + 3;
+        }else if (me == 2) { // draw
+            return accumulator + opponent + 3;
+        }else{ //win
+            if (opponent == 1) {
+                return accumulator + 2 + 6;
+            }else if (opponent == 2) {
+                return accumulator + 3 + 6;
+            }else {
+                return accumulator + 1 + 6;
+            }
+        }
+    });
+}
+
 int run_day_2(){
     std::cout << "--Day 2 START--" << std::endl;
     std::vector<std::pair<char,char>> rounds;
@@ -31,6 +61,7 @@ int run_day_2(){
     while (file >> currentRound.first >> currentRound.second) {
         rounds.push_back(currentRound);
     }
-    std::cout << "The total score while following the strategy guide is " << calculate_strategy_score(rounds) << "\n";
+    std::cout << "calculate_strategy_score " << calculate_strategy_score(rounds) << "\n";
+    std::cout << "calculate_new_strategy_score " << calculate_new_strategy_score(rounds) << "\n";
     std::cout << "--Day 2 END--\n" << std::endl;
 }
