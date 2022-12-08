43017cc584fda58989f14ef00616b52e85691b92
Andrej Pištek <andrej.pistek@outlook.com>
2022-12-08 13:22:12 +0100

    day 3 part 2

--- day3/day3.cpp
+++ day3/day3.cpp
@@ -16,14 +16,36 @@ int sum_item_priorities_of_both_compartments(const std::vector<std::string>& lin
         std::string firstHalf = line.substr(0, line.length()/2);
         std::string secondHalf = line.substr(line.length()/2);
         for (char const &c: firstHalf){ // m/2
-            if (secondHalf.find(c) != std::string::npos && // m/2
-            std::find(registered.begin(), registered.end(), c) == registered.end()){ // O(m/2)
+            if (secondHalf.find(c) != std::string::npos && // O(m/2)
+                std::find(registered.begin(), registered.end(), c) == registered.end()){ // O(m/2)
                 lineSum += isupper(c) ? c - 'A' + 27 : c - 'a' + 1;
                 registered.push_back(c);
             }
         }
         return accumulator + lineSum;
     });
+}
+
+int sum_grouped_item_priorities_of_both_compartments(const std::vector<std::string>& lines){
+    int groupSum = 0, groupIndex = 1;
+    std::vector<std::string> group;
+    std::vector<char> registered;
+    auto transformer = [&groupSum, &groupIndex, &group, &registered](auto accumulator, auto& line){ // n
+        if (groupIndex == 4){ groupIndex = 1; groupSum = 0; group.clear(); registered.clear();}
+        if (groupIndex == 3) {
+            for (char const &c: line) { // m
+                if (group[0].find(c) != std::string::npos && group[1].find(c) != std::string::npos &&
+                    std::find(registered.begin(), registered.end(), c) == registered.end()) {
+                    groupSum += isupper(c) ? c - 'A' + 27 : c - 'a' + 1;
+                    registered.push_back(c);
+                }
+            }
+        }
+        if (groupIndex == 1 || groupIndex == 2) group.push_back(line);
+        groupIndex++;
+        return accumulator + groupSum;
+    };
+    return std::accumulate(lines.begin(), lines.end(), 0, transformer);
 }
 
 int run_day_3(){
@@ -41,7 +63,8 @@ int run_day_3(){
         lines.push_back(line);
     }
 
-    std::cout << "sum_item_priorities_of_both_compartments: " << sum_item_priorities_of_both_compartments(lines) << "\n";
+    std::cout << "sum_item_priorities_of_both_compartments:         " << sum_item_priorities_of_both_compartments(lines) << "\n";
+    std::cout << "sum_grouped_item_priorities_of_both_compartments: " << sum_grouped_item_priorities_of_both_compartments(lines) << "\n";
 
     std::cout << "--Day " << DAY_NR << " END--\n" << std::endl;
     return 0;
