f576478f50b30ac0ecf74851a41e69d0794fd5e4
Andrej Pištek <andrej.pistek@outlook.com>
2022-12-08 14:58:39 +0100

    boilerplate day4

--- day4/day4.cpp
+++ day4/day4.cpp
@@ -3,6 +3,20 @@
 std::string const DAY_NR = "4";
 
 int run_day_4(){
+    std::string const fileName = "day" + DAY_NR + ".txt";
+    std::cout << "--Day " << DAY_NR << " START--" << std::endl;
+    std::ifstream file(fileName);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open " << std::quoted(fileName) << "\n";
+        return 1;
+    }
 
+    std::vector<std::string> lines;
+    std::string line;
+    while (getline(file, line)) {
+        lines.push_back(line);
+    }
+
+    std::cout << "--Day " << DAY_NR << " END--\n" << std::endl;
     return 0;
 }
