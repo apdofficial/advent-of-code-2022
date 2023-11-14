956f9e40eca18e41c80a1b44f311f7029c571f44
Andrej Pištek <andrej.pistek@outlook.com>
2023-11-14 22:21:31 +0100

    clean up and added CI

--- src/include/util.h
+++ src/include/util.h
@@ -5,15 +5,15 @@
 #include <optional>
 #include <string_view>
 #include <system_error>
-#include <vector>
+#include <array>
 #include <charconv>
 #include <iostream>
 
 namespace aoc
 {
-    auto stooi(std::string_view line) -> std::optional<int>;
+    constexpr auto stooi(std::string_view line) -> std::optional<int>;
 
-    auto stooi(std::span<const std::string> lines) -> std::vector<std::optional<int>>;
+    auto stooi(std::span<std::string> lines) -> std::vector<std::optional<int>>;
 
     auto measure_average_runtime(const std::function<void()>& fn, std::size_t n_runs) -> std::chrono::milliseconds;
 }
--- test/day1_test.cpp
+++ test/day1_test.cpp
@@ -1,25 +1,21 @@
 #include <catch2/catch_all.hpp>
 #include <fmt/format.h>
-
-#include <chrono>
+#include <array>
 
 #include "day1.hpp"
 #include "util.h"
 #include "File.h"
 
-
 using namespace aoc;
 
+std::vector<std::string> short_lines{"1000", "2000", "3000", "",
+                                     "4000", "",
+                                     "5000", "6000", "",
+                                     "7000", "8000", "9000","",
+                                     "10000"};
+auto short_calories{stooi(short_lines)};
 
-std::vector<std::string> short_lines {
-                                "1000", "2000", "3000", "",
-                                "4000", "",
-                                "5000", "6000", "",
-                                "7000", "8000", "9000", "",
-                                "10000"};
-std::vector<std::optional<int>> short_calories { aoc::stooi(short_lines) };
-
-constexpr std::string_view DATA_PATH = "/Users/andrejpistek/Developer/GitHub/advent-of-code-2022/data/day1.txt";
+constexpr std::string_view DATA_PATH = "../../data/day1.txt";
 
 TEST_CASE("Day 1, part 1 test") {
     auto result = day1::top_1_elf_calories(short_calories);
@@ -62,7 +58,7 @@ TEST_CASE("Day 1 file (correctness)") {
 }
 
 TEST_CASE("Day 1 file (performance)") {
-    constexpr std::size_t n_runs{100000u};
+    constexpr std::size_t n_runs{10000u};
 
     File file(DATA_PATH);
     auto lines = file.read_lines();
