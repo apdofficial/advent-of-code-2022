f2c36bebdc61a44f034976d79b018e281c72b94c
Andrej Pištek <andrej.pistek@outlook.com>
2023-11-17 22:33:35 +0100

    clean up of code

--- src/day8.cpp
+++ src/day8.cpp
@@ -1,8 +1,5 @@
 #include "day8.hpp"
 
-#include <ranges>
-#include <algorithm>
-
 // 3  0   3   7   3
 // 2 [5] [5]  1   2
 // 6 [5]  3  [3]  2
@@ -10,42 +7,37 @@
 // 3  5   3   9   0
 
 auto aoc::day8::count_visible_trees(const Forest &forest) -> unsigned {
+    // we assume forest is a square matrix
     std::unordered_set<unsigned> visible_inner_trees{};
-    // we assume a square matrix is given
-    unsigned visible_outer_trees_num = forest.size() * 4 - 4; // top + bottom + lef + right - 4 duplicate corners
-    // we assume a square matrix is given
-    for (std::size_t i = 1; i < forest.size() - 1; ++i) {
+    auto update_visible_inner_trees = [&forest, &visible_inner_trees](auto &i, auto &j, auto &tallest) {
+        if (forest[i][j] > tallest) {
+            tallest = forest[i][j];
+            visible_inner_trees.insert(forest.size() * i + j);
+        }
+    };
+    // left-to-right and right-to-left
+    for (unsigned i = 1; i < forest.size() - 1; ++i) {
         unsigned tallest_left = forest[i][0];
         for (std::size_t j = 1; j < forest.size() - 1; ++j) {
-            if (forest[i][j] > tallest_left) {
-                tallest_left = forest[i][j];
-                visible_inner_trees.insert(forest.size() * i + j);
-            }
+            update_visible_inner_trees(i, j, tallest_left);
         }
         unsigned tallest_right = forest[i][forest.size() - 1];
         for (int j = (int) forest.size() - 1; j > 0; --j) {
-            if (forest[i][j] > tallest_right) {
-                tallest_right = forest[i][j];
-                visible_inner_trees.insert(forest.size() * i + j);
-            }
+            update_visible_inner_trees(i, j, tallest_right);
         }
     }
-    for (std::size_t j = 1; j < forest.size() - 1; ++j) {
+    // top-down and bottom-to-top
+    for (unsigned j = 1; j < forest.size() - 1; ++j) {
         unsigned tallest_top = forest[0][j];
         for (unsigned i = 1; i < forest.size() - 1; ++i) {
-            if (forest[i][j] > tallest_top) {
-                tallest_top = forest[i][j];
-                visible_inner_trees.insert(forest.size() * i + j);
-            }
+            update_visible_inner_trees(i, j, tallest_top);
         }
         unsigned tallest_bottom = forest[forest.size() - 1][j];
         for (int i = (int) forest.size() - 1; i > 0; --i) {
-            if (forest[i][j] > tallest_bottom) {
-                tallest_bottom = forest[i][j];
-                visible_inner_trees.insert(forest.size() * i + j);
-            }
+            update_visible_inner_trees(i, j, tallest_bottom);
         }
     }
-    return visible_inner_trees.size() + visible_outer_trees_num;
+    // inner + outer (top + bottom + lef + right - 4 duplicate corners)
+    return visible_inner_trees.size() + forest.size() * 4 - 4;
 }
 
