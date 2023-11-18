8a797148648afb7c39e5ecbc1230d51aa954d762
Andrej Pištek <andrej.pistek@outlook.com>
2023-11-18 12:52:07 +0100

    day 9 prep

---
+++ test/day9_test.cpp
@@ -0,0 +1,35 @@
+#include <catch2/catch_all.hpp>
+#include <fmt/format.h>
+
+#include "day9.hpp"
+#include "util.h"
+#include "file.hpp"
+
+using namespace aoc::day9;
+
+std::vector<std::string> short_forest_d8{"R 4",
+                                         "U 4",
+                                         "L 3",
+                                         "D 1",
+                                         "R 4",
+                                         "D 1",
+                                         "L 5",
+                                         "R 2"};
+
+constexpr std::string_view DATA_PATH = "../../data/day9.txt";
+
+
+TEST_CASE("Day 9, part 1 test") {
+    REQUIRE(false);
+}
+
+TEST_CASE("Day 9, part 2 test") {
+    REQUIRE(false);
+}
+
+TEST_CASE("Day 9 file (correctness)") {
+    aoc::File file(DATA_PATH);
+    REQUIRE(file.is_valid());
+
+    REQUIRE(false);
+}
